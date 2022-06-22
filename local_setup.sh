337e239bb691d73f5fb8e92c22a157454f90c646
Parallel Works app-run user <sfg3@mgmt-sfg3-awsv2-00033.pw-canary-us-east-1.pw.local>
2022-06-22 05:14:37 +0000

    WRF running but very slow - only 3 CPUs/worker used

--- local_setup.sh
+++ local_setup.sh
@@ -4,14 +4,29 @@
 # a worker node's image
 # (the head node image
 # is always the bare bones)
+#
+# The spack compiler lines are
+# also need to setup the location
+# of gcc and intel compilers in
+# $HOME/.spack.  Since this
+# setup is on $HOME, it only
+# needs to be done once and
+# it applies to the other
+# worker nodes on the cluster.
 #========================
 
 cd $HOME
 
 cat <<EOF > srun_setup_script.sh
 #!/bin/bash
-echo 'source ~/.bashrc; rsync -av /var/lib/pworks/conus_12km $HOME/; cd $HOME/conus_12km/; spack loca\
-tion -i wrf%intel | xargs -I@ sh -c "ln -s @/test/em_real/* ."' | scl enable devtoolset-7 bash   
+echo 'source ~/.bashrc; \
+rsync -av /var/lib/pworks/conus_12km $HOME/; \
+cd $HOME/conus_12km/; \
+spack compiler find; \
+spack load intel-oneapi-compilers; \
+spack compiler find; \
+spack unload; \
+spack location -i wrf%intel | xargs -I@ sh -c "ln -s @/test/em_real/* ."' | scl enable devtoolset-7 bash   
 
 EOF
 
