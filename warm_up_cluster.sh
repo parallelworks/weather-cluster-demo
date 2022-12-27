f1c1cc2c170982c1991eb56a84741d7e94deea45
StefanGary <sfgary@parallelworks.com>
2022-12-27 12:56:48 +0000

    Tested working cluster warm up script

--- warm_up_cluster.sh
+++ warm_up_cluster.sh
@@ -10,20 +10,13 @@
 # Set the number of workers
 # to warm up and warm up time
 n_warm_workers=8
-
-# Create temporary run script
-cat <<EOF > node_commands.tmp
-echo Started $hostname
-sleep 99
-EOF
-
-chmod a+rwx node_commands.tmp
+warm_time=99
 
 # Broadcast to workers and run
 # https://slurm.schedmd.com/srun.html
-srun --bcast=/tmp -N${n_warm_workers} node_commands.tmp
+#srun --bcast=/home/sfgary -N${n_warm_workers} node_commands.tmp
 
-# Clean up
-rm -rf node_commands.tmp
+srun -N${n_warm_workers} hostname
+srun -N${n_warm_workers} sleep $warm_time
 
 echo Done with warm_up_cluster
