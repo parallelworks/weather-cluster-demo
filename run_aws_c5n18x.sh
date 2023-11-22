58e3e2b2be363fee4fdf2db71d0fd41d56400b21
lpeffers <luke.peffers@gmail.com>
2023-11-22 21:51:43 +0000

    Small adjustment to aws c5n18 run script and moved plotting example to examples directory

--- run_aws_c5n18x.sh
+++ run_aws_c5n18x.sh
@@ -31,7 +31,7 @@ source ~/.bashrc
 
 # AV: Modifying this to run from wherever the repo is being launched. Use workflow to specify this location.
 # cd /shared/wrf/conus_12km/
-cd conus_12km
+cd $HOME/wrf/conus_12km
 
 export I_MPI_FABRIC=efa
 
