8eec2f6e57787599fefcbdc9a6f4f37838ed5d97
Parallel Works app-run user <sfgary@mgmt-sfgary-gce2-00008.c.modular-magpie-167320.internal>
2024-11-06 19:25:54 +0000

    Add WRF to Spack buildcache

---
+++ install/step_02_deploy_to_node_from_buildcache.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#==============================
+# System setup script to use an
+# existing Spack stack. These 
+# commands are basically the same
+# as the start of build_node_image.sh.
+#
+# Please see README.md for how how/why
+# to use install_dir, below.
+#==============================
+
+install_dir=${HOME}/wrf
+#install_dir=/var/lib/pworks
+
+#==============================
+echo Install newer version of gcc...
+#==============================
+
+sudo yum install -y centos-release-scl
+sudo yum install -y devtoolset-7
+
+#==============================
+echo Setting up SPACK_ROOT...
+#==============================
+
+export SPACK_ROOT=${install_dir}/spack
+sudo mkdir -p $SPACK_ROOT
+sudo chmod --recursive a+rwx ${install_dir}
+
+#==============================
+echo Downloading spack...
+#==============================
+
+git clone -b v0.18.0 -c feature.manyFiles=true https://github.com/spack/spack $SPACK_ROOT
+
+#==============================
+echo Configuring external packages...
+#==============================
+
+spack_packages=${SPACK_ROOT}/etc/spack/packages.yaml
+echo "packages:" > $spack_packages
+echo "    gcc:" >> $spack_packages
+echo "        externals:" >> $spack_packages
+echo "        - spec: gcc@7.3.1" >> $spack_packages
+echo "          prefix: /opt/rh/devtoolset-7/root/usr" >> $spack_packages
+echo "        buildable: False" >> $spack_packages
+echo "    slurm:" >> $spack_packages
+echo "        variants: +pmix sysconfdir=/mnt/shared/etc/slurm" >> $spack_packages
+echo "        externals:" >> $spack_packages
+echo "        - spec: slurm@20.02.7 +pmix sysconfdir=/mnt/shared/etc/slurm" >> $spack_packages
+echo "          prefix: /usr" >> $spack_packages
+echo "        buildable: False" >> $spack_packages
+
+#==============================
+echo Set up Spack environment...
+#==============================
+
+# An alternative for local testing is $HOME/.bashrc
+# Here, this is added to /etc/bashrc so it is persistent
+# in the image and $HOME/.bashrc sources /etc/bashrc.
+sudo -s eval echo export SPACK_ROOT=${SPACK_ROOT}" >> "/etc/bashrc
+sudo -s eval echo source ${SPACK_ROOT}/share/spack/setup-env.sh" >> "/etc/bashrc
+source $HOME/.bashrc
+
+#==============================
+echo Install some dependencies to check download certificates...
+#==============================
+
+pip3 install botocore==1.23.46 boto3==1.20.46
+
+#==============================
+echo Adding mirror with buildcache...
+#==============================
+./step_03_add_buildcache.sh
+
+#==============================
+echo Set up Spack...
+#==============================
+# spack install -j nproc is set to -j 30
+# because 30 CPU is the typical minimum
+# amount of CPU for high-speed networking
+# instance types.
+echo 'source ~/.bashrc; \
+spack compiler find; \
+spack install -j 30 patchelf; \
+spack install -j 30 intel-oneapi-compilers@2022.1.0; \
+spack load intel-oneapi-compilers; \
+spack compiler find; \
+spack unload; ' | scl enable devtoolset-7 bash
+
+#==============================
+echo Set permissions...
+#==============================
+sudo chmod --recursive a+rwx $install_dir
+
+#==============================
+echo Make links in model data...
+#==============================
+# This is the only step in local_setup.sh
+# not already done in the Spack stack
+# or here.
+cd $install_dir
+cd conus_12km
+spack location -i wrf%intel | xargs -I@ sh -c "ln -s @/test/em_real/* ."
+
+echo Completed deploying to cluster
---
+++ install/step_04_push_to_buildcache.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#==========================
+# Push Spack installation
+# to buildcache.
+#
+# This script assumes that
+# Spack is already installed
+# and $SPACK_ROOT is defined.
+#==========================
+
+# The _name is used by Spack.
+# The _id corresponds to the bucket
+# available on the PW CLI.
+export SPACK_BUILDCACHE_NAME="wrf-cache"
+export SPACK_BUILDCACHE_ID="pw://sfgary/spackwrf"
+
+# Use the PW CLI to get the bucket credentials
+# and the $BUCKET_URI, used to set up the
+# buildcache below.
+eval `pw buckets get-token ${SPACK_BUILDCACHE_ID}`
+
+# "Start" Spack
+source ${SPACK_ROOT}/share/spack/setup-env.sh
+
+# For each installed package, push to cache
+for ii in $(spack find --format "yyy {version} /{hash}" |
+            grep -v -E "^(develop^master)" |
+            grep "yyy" |
+            cut -f3 -d" ")
+do
+  echo Working on $ii
+  spack buildcache create -af --only=package --unsigned --mirror-name ${SPACK_BUILDCACHE_NAME} $ii
+done
+
+# After a push, you need to:
+# This line applies to newer versions of Spack (v.20+?)
+# where you can reference the buildcache by it's Spack name.
+#
+#spack buildcache update-index ${SPACK_BUILDCACHE_NAME}
+#
+# For older versions of Spack, you need to use the URL:
+#
+spack buildcache update-index --mirror-url $BUCKET_URI
+
