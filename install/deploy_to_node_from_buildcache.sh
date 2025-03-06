cf23d5a6de459b6a20ea94b5fe7e8c8b015eb1e4
Stefan Gary <stefanfgary@gmail.com>
2025-03-06 16:18:45 +0000

    Reduce Spack path padding from 512 to 256

--- install/deploy_to_node_from_buildcache.sh
+++ install/deploy_to_node_from_buildcache.sh
@@ -168,7 +168,7 @@ source /opt/rh/gcc-toolset-${gcc_version}/enable
 #==============================
 # Need to specify a long path padding to
 # allow for different install locations.
-spack config add config:install_tree:padded_length:512
+spack config add config:install_tree:padded_length:256
 
 #=============================
 echo Adding buildcache...
