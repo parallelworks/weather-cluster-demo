bb7bdcd8d9244d9dd04959a524b09066f408e958
Stefan Gary <stefanfgary@gmail.com>
2025-03-06 18:12:11 +0000

    Disable Spack path padding

--- install/deploy_to_node_from_buildcache.sh
+++ install/deploy_to_node_from_buildcache.sh
@@ -173,7 +173,10 @@ source /opt/rh/gcc-toolset-${gcc_version}/enable
 #==============================
 # Need to specify a long path padding to
 # allow for different install locations.
-spack config add config:install_tree:padded_length:256
+# WORKING HERE:
+# If this is 512, Intel OneAPI fails to install
+# lz4 is failing to install with any value of this setting.
+#spack config add config:install_tree:padded_length:256
 
 #=============================
 echo Adding buildcache...
